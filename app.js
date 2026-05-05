38245c04023c3c5765141b67ac859d05d102a072
TumharaGitHubName <hassaanali994@gmail.com>
2026-05-05 17:27:32 +0500

    Add game logic and UI for Rock-Paper-Scissors game

---
+++ app.js
@@ -0,0 +1,62 @@
+let userScore = 0;
+let compScore = 0;
+
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score")
+
+
+const genCompChoice = (() => {
+    let option = ["rock", "paper", "scissors"];
+    let ranIdx = Math.floor(Math.random() * 3);
+    return option[ranIdx]
+})
+
+
+const drawGame = (() => {
+    msg.innerText = "Game was draw."
+})
+
+
+const showWinner = ((userWinner, userChoice, compChoice) => {
+    if (userWinner) {
+        userScore++
+        userScorePara.innerText = userScore
+        msg.innerText = `You win! your ${userChoice} beats ${compChoice}`
+    } else {
+        compScore++
+        compScorePara.innerText = compScore
+        msg.innerText = `You lose! ${compChoice} beats your ${userChoice}`
+    }
+})
+
+
+const playGame = ((userChoice) => {
+    console.log(`user choice ${userChoice}`);
+    let compChoice = genCompChoice()
+    console.log(`computer choice ${compChoice}`);
+
+    if (userChoice === compChoice) {
+        drawGame()
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice)
+    }
+})
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id")
+        playGame(userChoice)
+    })
+})
